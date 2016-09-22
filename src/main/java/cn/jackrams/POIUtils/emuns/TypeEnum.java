4fc683fd0950a913ddf1b6978f2453a834ad317f
Harrytion <764019983@qq.com>
2016-09-22 22:56:38 +0800

    Add files via upload

---
+++ src/main/java/cn/jackrams/POIUtils/emuns/TypeEnum.java
@@ -0,0 +1,5 @@
+package cn.jackrams.POIUtils.emuns;
+
+public enum TypeEnum {
+ String , Date ,Double ,Integer,Float ,Boolean, Character,ByteArray
+}
---
+++ src/main/java/cn/jackrams/POIUtils/emuns/ViewType.java
@@ -0,0 +1,10 @@
+package cn.jackrams.POIUtils.emuns;
+
+public enum ViewType {
+   String  //字符串
+   ,Number //数字
+   ,Percent //百分比
+   ,Money  //金钱
+   ,Date
+   ,Image
+}
